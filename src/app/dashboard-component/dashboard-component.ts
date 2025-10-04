14c832463a8c20984f274daa801884324222cb51
pcwdeveloper <118196185+pcwdeveloper@users.noreply.github.com>
2025-10-04 14:36:43 +0530

    Add Dashboard

--- src/app/dashboard-component/dashboard-component.ts
+++ src/app/dashboard-component/dashboard-component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { DashboardService } from '../services/dashboard-service';
 
 @Component({
   selector: 'app-dashboard-component',
-  imports: [],
+  imports: [CommonModule,
+    MatCardModule,
+    MatIconModule,],
   templateUrl: './dashboard-component.html',
   styleUrl: './dashboard-component.css'
 })
 export class DashboardComponent {
 
+  dashboardService: DashboardService = inject(DashboardService);
+  // Using signals (new reactive primitive in Angular) for state
+  totalUsers = 0;
+  activeUsers = 0;
+  inActiveUsers = 0;
+  isLoading = false;
+  constructor() {
+    this.getDashboard();
+  }
+
+  getDashboard() {
+    this.isLoading = true;
+    // Simulate API fetch — replace with your HTTP call
+    this.dashboardService.getDashboard().subscribe(res => {
+      this.isLoading = false;
+      this.totalUsers = res.totalUser;
+      this.activeUsers = res.activeUser;
+      this.inActiveUsers = res.inActiveUser;
+    },
+     (err) => {
+      this.isLoading = false;
+      console.error('Error get user:', err);
+    });
+  }
 }
---
+++ src/app/services/dashboard-service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../model/user';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardService {
+  
+  private http = inject(HttpClient);
+
+  getDashboard(): Observable<any>{
+    return this.http.get<any>(environment.apiBaseUrl+'api/dashboard');
+  }
+}
